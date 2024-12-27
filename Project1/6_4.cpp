691faddfb1f76e0a38d6e73e3ad9154f8461d24b
jhaej22 <jhaej0722@gmail.com>
2024-12-27 14:16:58 +0900

    Add files via upload

---
+++ Project1/6_4.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* int main(void) {
+
+	int type, point;
+
+	printf("번호를 선택: 1(1종 면허), 2(2종 면허): ");
+	scanf("%d", &type);
+	printf("필기시험 점수 입력: ");
+	scanf("%d", &point);
+
+	if (type == 1) {
+		if (point >= 70) {
+			printf("1종 면허시험 합격\n");
+		}
+		else {
+			printf("1종 면허시험 불합격\n");
+		}
+	}
+	else {
+		if (point >= 60) {
+			printf("2종 면허시험 합격\n");
+		}
+		else {
+			printf("2종 면허시험 불합격\n");
+		}
+	}
+
+	return 0;
+} */
---
+++ Project1/6_5.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int age = 0;
+
+	printf("당신의 나이는?: ");
+	scanf("%d", &age);
+
+	if (age >= 20) {
+		if (age >= 65) {
+			printf("어르신 입니다.\n");
+		}
+		else {
+			printf("성인 입니다.\n");
+		}
+	}
+	else {
+		printf("미성년자 입니다.\n");
+	}
+
+	return 0;
+}
